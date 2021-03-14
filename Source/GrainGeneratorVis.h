8272318fc9d03b5628297d1b9603b2a00c4e7545
Alex Rycroft <alex.rycroft@gmail.com>
2021-03-14 18:31:00 +0000

    grain gen mouse click slider

--- Source/GrainGeneratorVis.h
+++ Source/GrainGeneratorVis.h
@@ -12,13 +12,20 @@
 #include <JuceHeader.h>
 #include "DraggableComponent.h"
 
-class GrainGeneratorVis : public DraggableComponent
+class GrainGeneratorVis : public DraggableComponent, public juce::Slider::Listener
 {
 public:
 	GrainGeneratorVis(juce::ValueTree vTree)
 		: DraggableComponent(vTree),
 		paramTree(vTree)
 	{
+		this->addAndMakeVisible(frequencySlider);
+		frequencySlider.setRange(50, 5000.0);          
+		frequencySlider.setTextValueSuffix(" Hz");
+		frequencySlider.setSliderSnapsToMousePosition(false);
+		frequencySlider.setInterceptsMouseClicks(false, false);
+		frequencySlider.setSliderStyle(juce::Slider::SliderStyle::LinearVertical);
+		frequencySlider.addListener(this);
 	}
 
 	void paint(juce::Graphics& g) override
@@ -31,13 +38,53 @@ public:
 
 	void resized() override
 	{
+		frequencySlider.setBounds(0, 0, getWidth() * 4, getHeight() * 4);
 	}
 
-	juce::ValueTree getValueTree() 
+	juce::ValueTree getValueTree()
 	{
 		return paramTree;
 	}
 
+	void mouseDown(const juce::MouseEvent& event) override
+	{
+		if(event.mods.isRightButtonDown())
+		{
+			frequencySlider.mouseDown(event);
+		}
+		else
+		{
+			DraggableComponent::mouseDown(event);
+		}
+	}
+
+	void mouseDrag(const juce::MouseEvent& event) override
+	{
+		if (event.mods.isRightButtonDown())
+		{
+			frequencySlider.mouseDrag(event);
+		}
+		else
+		{
+			DraggableComponent::mouseDrag(event);
+		}
+	}
+
+	void mouseUp(const juce::MouseEvent& event) override
+	{
+		/*if (event.mods.isRightButtonDown())
+		{
+			frequencySlider.setInterceptsMouseClicks(false, false);
+		}*/
+	}
+
+	void sliderValueChanged(juce::Slider* slider) override
+	{
+		paramTree.setProperty(Ids::frequency, slider->getValue(), nullptr);
+
+	}
 private:
 	juce::ValueTree paramTree;
+	juce::Slider frequencySlider;
+
 };
